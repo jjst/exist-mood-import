4fad5fd28cce8c0df57c88e612032843ce0c30e4
Jeremie Jost <jeremiejost@gmail.com>
2018-10-06 14:50:21 +0100

    Successful import!

--- imoodjournal.py
+++ imoodjournal.py
@@ -2,9 +2,22 @@ from collections import namedtuple
 from datetime import datetime
 import csv
 import sys
-import webbrowser
+import requests, json
+import auth
+import operator
+import itertools
+
 
 Mood = namedtuple('Mood', ['date', 'level', 'comment', 'tags'])
+
+MoodData = namedtuple('MoodData', ['mood_tags', 'moods'])
+
+def combine(date, moods):
+    moods = list(moods)
+    new_level = sum(m.level for m in moods) // len(moods)
+    tags = set(tag for m in moods for tag in m.tags)
+    comment = ' '.join(m.comment for m in moods).strip()
+    return Mood(date, new_level, comment, tags)
 
 def mood_from_row(row, mood_tags):
     date_string = row[0]
@@ -14,7 +27,7 @@ def mood_from_row(row, mood_tags):
         return None
     active_tags = [int(i) for i in row[7:]]
     active_indices = [i for (i, t) in enumerate(active_tags) if t == 1]
-    tags = [mood_tags[i] for i in active_indices]
+    tags = set([mood_tags[i] for i in active_indices])
     normalized_level = int(row[4]) // 2
     return Mood(date=d, level=normalized_level, comment=row[6], tags=tags)
 
@@ -23,12 +36,67 @@ def import_moods_from_csv(csv_file_name):
         reader = csv.reader(csvfile)
         headers = next(reader)
         mood_tags = headers[7:]
-        return [mood_from_row(row, mood_tags) for row in reader]
+        all_moods = [mood_from_row(row, mood_tags) for row in reader]
+        unique_moods_per_day = [combine(date, moods) for (date, moods) in itertools.groupby(all_moods, operator.attrgetter('date'))]
+        return MoodData(mood_tags, unique_moods_per_day)
+
+def acquire_attrs(attributes, token):
+    url = 'https://exist.io/api/1/attributes/acquire/'
+
+    attrs = [{"name": a, "active": True} for a in attributes]
+
+    response = requests.post(url, headers={'Authorization':f"Bearer {token}"},
+        json=attrs)
+    return response
+
+def release_attrs(attributes, token):
+    url = 'https://exist.io/api/1/attributes/release/'
+
+    attrs = [{"name": a} for a in attributes]
+
+    response = requests.post(url, headers={'Authorization':f"Bearer {token}"},
+        json=attrs)
+    return response
+
+def publish_data(moods, token):
+    for mood in moods:
+        response = publish_one(mood, token)
+        print(response)
+        print(response.json())
+
+
+def publish_one(mood, token):
+    url = 'https://exist.io/api/1/attributes/update/'
+    def create_attr(name, value, date):
+        date_format = '%Y-%m-%d'
+        return {"name": name, "date": date.strftime(date_format), "value": value}
+    def attributes(mood):
+        attrs = [create_attr("mood", mood.level, mood.date)]
+        if mood.comment:
+            attrs.append(create_attr("mood_note", mood.comment, mood.date))
+        if mood.tags:
+            attrs.append(create_attr("custom", ", ".join(mood.tags), mood.date))
+        return attrs
+    response = requests.post(url, headers={'Authorization':f"Bearer {token}"},
+        json=attributes(mood))
+    return response
+
+
+
+def do_import(mood_data_file, token):
+    mood_data = import_moods_from_csv(mood_data_file)
+    attrs = ["mood", "mood_note", "custom"]
+    try:
+        acquire_attrs(attrs, token)
+        publish_data(mood_data.moods, token)
+    finally:
+        release_attrs(attrs, token)
+
 
 def main():
     filename = sys.argv[1]
-    moods = import_moods_from_csv(filename)
-    print(moods)
+    token = auth.token()
+    do_import(filename, token)
 
 if __name__ == '__main__':
     main()
