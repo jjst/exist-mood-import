273987267304a17946ac5d44aee5e1e6d4febaab
Jeremie Jost <jeremiejost@gmail.com>
2018-10-06 13:39:49 +0100

    Normalize mood level

--- imoodjournal.py
+++ imoodjournal.py
@@ -15,7 +15,8 @@ def mood_from_row(row, mood_tags):
     active_tags = [int(i) for i in row[7:]]
     active_indices = [i for (i, t) in enumerate(active_tags) if t == 1]
     tags = [mood_tags[i] for i in active_indices]
-    return Mood(date=d, level=int(row[4]), comment=row[6], tags=tags)
+    normalized_level = int(row[4]) // 2
+    return Mood(date=d, level=normalized_level, comment=row[6], tags=tags)
 
 def import_moods_from_csv(csv_file_name):
     with open(csv_file_name) as csvfile:
@@ -24,13 +25,10 @@ def import_moods_from_csv(csv_file_name):
         mood_tags = headers[7:]
         return [mood_from_row(row, mood_tags) for row in reader]
 
-def authorize_app():
-    webbrowser.open('https://exist.io/oauth2/authorize?response_type=code&client_id=%s&scope=%s' % (CLIENT_ID, "read+write"))
-
 def main():
     filename = sys.argv[1]
-    authorize_app()
     moods = import_moods_from_csv(filename)
+    print(moods)
 
 if __name__ == '__main__':
     main()
